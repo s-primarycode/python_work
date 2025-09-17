6970538ef46439739e01f79d05aeb1e232ff6b30
s-primarycode <154287418+s-primarycode@users.noreply.github.com>
2025-09-16 21:23:31 -0700

    Page 3 - 20
    Part 1 - Basics
         Chapter 1 - Getting Started
         Chapter 2 - Variable and Simple Data
                            types

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+message = "Hello Python Crash Course reader!"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
