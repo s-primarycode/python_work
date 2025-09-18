a081402c0e8e8217e45c082acb433a0202ecc37d
s-primarycode <154287418+s-primarycode@users.noreply.github.com>
2025-09-17 20:17:35 -0700

    Page 21 - 22
    Completed page 21 - 22. Code no longer used was made into comment

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,30 @@
-message = "Hello Python world!"
-print(message)
+#message = "Hello Python world!"
+#print(message)
 
-message = "Hello Python Crash Course world!"
-print(message)
+#message = "Hello Python Crash Course world!"
+#print(message)
 
-message = "Hello Python Crash Course reader!"
-print(message)
+#message = "Hello Python Crash Course reader!"
+#print(message)
 
-name = "ada lovelace"
-print(name.title())
+#name = "ada lovelace"
+#print(name.title())
 
-name = "Ada Lovelace"
-print(name.upper())
-print(name.lower())
+#USING VARIABLES IN STRINGS
+
+#name = "Ada Lovelace"
+#print(name.upper())
+#print(name.lower())
+
+#first_name = "ada"
+#last_name = "lovelace"
+#full_name = f"{first_name} {last_name}"
+#print(full_name)
+
+#first_name = "ada"
+#last_name = "lovelace"
+#full_name = f"{first_name} {last_name}"
+#message = f"Hello, {full_name.title()}!"
+#print(message)
+
+#ADDING WHITESPACE TO STRINGS WITH TABS OR NEWLINES
